57f1cdc9a4e5744a76b80932cf17b8fc644f591a
Théo Santos <theo.dellacqua@gmail.com>
2024-09-30 09:11:02 -0300

    exercicio 1.1 com if implementado

---
+++ exercicios_operadores_logicos_relacionais.sql
@@ -0,0 +1,26 @@
+-- Active: 1727015344955@@127.0.0.1@5432@20242_fatec_ipi_pbdi_theo@public
+
+--Codigo de gerar valor aleatorio
+CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
+INT) RETURNS INT AS
+$$
+BEGIN
+RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+END;
+$$ LANGUAGE plpgsql;
+
+--Exercicio 1.1 - Fazendo com IF
+DO
+$$
+DECLARE
+    numero INT := valor_aleatorio_entre(0, 20);
+BEGIN
+    IF numero % 3 = 0 THEN
+        RAISE NOTICE 'O número % é multiplo de 3', numero;
+    ELSE
+        RAISE NOTICE 'O número % não é multiplo de 3', numero;
+    END IF;
+END;
+$$
+
+
